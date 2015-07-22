5670b81debb6bf34d7f68864836f25c911b2d0ae
977616908 <cdp@CDPdeMac-mini.lan>
2015-07-22 14:06:54 +0800

    ct

--- Root/Camera/controller/CameraViewController.m
+++ Root/Camera/controller/CameraViewController.m
@@ -37,7 +37,7 @@
 {
     [super viewWillAppear:animated];
     self.navigationController.navigationBarHidden=YES;
-    [self showConnect];
+//    [self showConnect];
 }
 
 -(void)createNav{
@@ -90,12 +90,12 @@
     [bgView addSubview:downMsg];
     
     UIView *stepView=[[UIView alloc]initWithFrame:CGRectMake(0, CGRectGetMaxY(downMsg.frame), 320, 150)];
-    CCImageView *imgConn=CCImageViewCreateWithNewValue(@"hm_camera_conn", CGRectMake(0, 0, 320, 150));
+    CCImageView *imgConn=CCImageViewCreateWithNewValue(@"hm_camera_conn", CGRectMake(20, 0, 320, 150));
     [stepView addSubview:imgConn];
     CCLabel *lbMsg=CCLabelCreateWithBlodNewValue(@"配制摄像头步骤:", 13.0, CGRectMake(15, 20, 100, 14));
     lbMsg.textColor=RGBCommon(155, 155, 155);
     [stepView addSubview:lbMsg];
-    NSArray *arr=@[@" 1.扫描设置二维码",@" 2.连接“IPCAM-XXX”的WIFI",@" 3.连接摄像头",@" 4.设置摄像头WIFI",@" 5.重启摄像头"];
+    NSArray *arr=@[@" 1.扫描二维码或局域网搜索设备",@" 2.连接“IPCAM-XXX”的WIFI",@" 3.连接摄像头",@" 4.设置摄像头WIFI",@" 5.重启摄像头"];
     arrSteps=[NSMutableArray array];
     for(int i=0;i < arr.count;i++){
         CGFloat hg=40+i*15;
@@ -151,48 +151,24 @@
             [self.navigationController pushViewController:svc animated:NO];
         }
     }
-
-
 }
-//判断是否连接当前摄像头
+
+#pragma -mark 判断是否连接当前摄像头
 -(void)showConnect{
     NSString *wifiiName=[self getWifiName];
     if ([wifiiName hasPrefix:@"IPCAM-"]) {
         isConnect=YES;
         CameraMessage *msg=[[CameraMessage alloc]init];
         msg.camdevicewifiname=wifiiName;
-        _cameraMsg=msg;
+        self.cameraMsg=msg;
         [self setStepsCount:2];
         [self.btnStart alterNormalTitle:@"开始智能连接"];
+    }else{
+        
     }
     NSLog(@"---[%@]---",wifiiName);
 }
 
-
-#pragma mark - Update Views
-- (void)startAnimation
-{
-    self.downMsg.hidden=NO;
-    self.downIndicator.hidden=NO;
-    downloadedBytes = 0;
-    timer=[NSTimer scheduledTimerWithTimeInterval:0.1 target:self selector:@selector(updateView) userInfo:nil repeats:YES];
-}
-
-- (void)updateView
-{
-    downloadedBytes+=arc4random()%5;
-    downloadedBytes=downloadedBytes>100?100:downloadedBytes;
-    _downMsg.text=[NSString stringWithFormat:@"正在为您连接摄像头...(%.0f%%)",downloadedBytes];
-    [_downIndicator updateWithTotalBytes:100 downloadedBytes:downloadedBytes];
-    if(downloadedBytes>=100){
-        [timer invalidate];
-        timer=nil;
-        self.btnStart.enabled=YES;
-        _downMsg.text=@"摄像头连接成功";
-        [self.pifiiDelegate pushViewDataSource:@(0)];
-        [self performSelector:@selector(exitCurrentController) withObject:nil afterDelay:0.7];
-    }
-}
 
 -(void)setStepsCount:(NSInteger)count{
     for (int i=0; i<count; i++) {
@@ -238,13 +214,15 @@
         if ([returnCode intValue]==200) {
             NSDictionary *data=response[@"data"];
             CameraMessage *msg=[[CameraMessage alloc]initWithData:data];
-            self.cameraMsg=msg;
             PSLog(@"%@",msg);
-            if(msg.isOpen){
+            if(msg&&msg.isOpen){
+                self.cameraMsg=msg;
                 isConnect=YES;
                 [self setStepsCount:5];
                 self.btnSearch.hidden=YES;
                 [self.btnStart alterNormalTitle:@"开始智能连接"];
+            }else{
+                [self showConnect];
             }
         }
     }
@@ -296,6 +274,31 @@
 -(void)viewWillDisappear:(BOOL)animated{
     [super viewWillDisappear:animated];
     self.navigationController.navigationBarHidden=NO;
+}
+
+#pragma mark - Update Views
+- (void)startAnimation
+{
+    self.downMsg.hidden=NO;
+    self.downIndicator.hidden=NO;
+    downloadedBytes = 0;
+    timer=[NSTimer scheduledTimerWithTimeInterval:0.1 target:self selector:@selector(updateView) userInfo:nil repeats:YES];
+}
+
+- (void)updateView
+{
+    downloadedBytes+=arc4random()%5;
+    downloadedBytes=downloadedBytes>100?100:downloadedBytes;
+    _downMsg.text=[NSString stringWithFormat:@"正在为您连接摄像头...(%.0f%%)",downloadedBytes];
+    [_downIndicator updateWithTotalBytes:100 downloadedBytes:downloadedBytes];
+    if(downloadedBytes>=100){
+        [timer invalidate];
+        timer=nil;
+        self.btnStart.enabled=YES;
+        _downMsg.text=@"摄像头连接成功";
+        [self.pifiiDelegate pushViewDataSource:@(0)];
+        [self performSelector:@selector(exitCurrentController) withObject:nil afterDelay:0.7];
+    }
 }
 
 -(CATransition *)customAnimation1:(UIView *)viewNum upDown:(BOOL )boolUpDown{
